17716d168c5655809d21d64b53b0b6f7b27626b3
Frederick Rohn <frederickrohn@Fredericks-Laptop.local>
2025-01-15 01:26:58 -0800

    added the message board page

--- next-practice/app/message-board/page.js
+++ next-practice/app/message-board/page.js
@@ -1,11 +1,11 @@
 import Link from "next/link"; // Import Link for navigation
 
-export default function Practice(){
+export default function MessageBoard(){
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center p-8">
-            <h1 className="text-2xl font-bold mb-4">Practice Page</h1>
+        <div className="min-h-screen flex flex-col items-center justify-start p-8">
+            <h1 className="text-2xl font-bold mb-4">The Message Board</h1>
             <p className="text-lg text-gray-600 mb-8">
-                This is your practice page. Start building here!
+                This is the Message Board. Write whatever you want here!
             </p>
             <div className="flex gap-4 items-center flex-col sm:flex-row">
                 <Link 
@@ -13,9 +13,9 @@ export default function Practice(){
                     className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
                 >Go back to Home Page</Link>
                 <Link 
-                    href="/message-board" 
+                    href="/practice" 
                     className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-                >Write on the Message Board</Link>
+                >Go back to the practice page</Link>
             </div>
         </div>
     )
