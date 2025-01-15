b81db62bb59c2c300ce01760aec5c5b7d6237f88
Frederick Rohn <frederickrohn@Fredericks-Laptop.local>
2025-01-15 02:55:12 -0800

    wrote the api calls, implemented message board list

---
+++ next-practice/app/message-board/MessageList.js
@@ -0,0 +1,23 @@
+"use client";   //component needs to be a client component for interactive parts
+                //not implemented yet
+
+export default function MessageList({ messages }){
+    return (
+        <div className='w-full max-w-2xl'>
+            {messages.length === 0 ? (
+                <p>No messages yet, be the first one to post!</p>
+            ) : (messages.map(message=>(
+                <div 
+                key={ message.id }
+                className="border p-4 mb-4 rounded shadow-sm bg-black"
+                >
+                    <p className='text-lg'>{message.content}</p>
+                    <small className='text-gray-500'>
+                        Posted by {message.username} in{" "}
+                        {message.category || "Uncategorized"}
+                    </small>
+                </div>
+            )))}
+        </div>
+    )
+};
--- next-practice/app/message-board/page.js
+++ next-practice/app/message-board/page.js
@@ -1,12 +1,22 @@
 import Link from "next/link"; // Import Link for navigation
+import MessageList from './MessageList';
 
-export default function MessageBoard(){
+export default async function MessageBoard(){
+
+    //this is server side data fetching, which takes advantage of Next's SSR capabilities
+    //in a MERN stack app, this would be useEffect and using the API call from there
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/messages`, {
+        cache: "no-store", // Ensure fresh data for every request
+      });
+    const messages = await res.json();
+
     return (
         <div className="min-h-screen flex flex-col items-center justify-start p-8">
             <h1 className="text-2xl font-bold mb-4">The Message Board</h1>
             <p className="text-lg text-gray-600 mb-8">
                 This is the Message Board. Write whatever you want here!
             </p>
+            <MessageList messages={messages}/>
             <div className="flex gap-4 items-center flex-col sm:flex-row">
                 <Link 
                     href="/" 
