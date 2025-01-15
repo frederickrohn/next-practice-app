7daf634f57155819a2aa0d5e00d7ad82887577f8
Frederick Rohn <frederickrohn@Fredericks-Laptop.local>
2025-01-15 10:28:42 -0800

    font-styling stuff

--- next-practice/app/message-board/MessageList.js
+++ next-practice/app/message-board/MessageList.js
@@ -9,10 +9,10 @@ export default function MessageList({ messages }){
             ) : (messages.map(message=>(
                 <div 
                 key={ message.id }
-                className="border border-[0.5px] border-gray-800 p-4 mb-4 rounded shadow-sm bg-transparent"
+                className="border border-[0.5px] border-gray-800 p-6 mb-4 rounded shadow-sm bg-transparent"
                 >
-                    <p className='text-lg'>{message.content}</p>
-                    <small className='text-gray-500'>
+                    <p className='text-s text-gray-350'>{message.content}</p>
+                    <small className='text-xs text-gray-800'>
                         Posted by {message.username} in{" "}
                         {message.category || "Uncategorized"}
                     </small>
